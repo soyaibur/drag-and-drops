923cf5ed9bbeec97050349bc51db787cfce991c9
soyaibur <ovisoyaiburrahman@gmail.com>
2022-02-23 17:45:08 +0600

    getDragAfterElement function is written

--- logic.js
+++ logic.js
@@ -8,7 +8,7 @@ const draggables = document.querySelectorAll('.draggable')
 const containers = document.querySelectorAll('.container')
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-
+ 
 
 // .....All EventListener go undernith here...........
 draggables.forEach(draggable=>{
@@ -19,17 +19,66 @@ draggables.forEach(draggable=>{
     draggable.addEventListener("dragend",()=>{
      draggable.classList.remove('dragging')
     })
-
+ 
 })
+
+
+
 
 for(let container of containers){
 
     container.addEventListener("dragover",(e)=>{
         e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        console.log(afterElement)
         const draggable = document.querySelector('.dragging')
         container.appendChild(draggable)
     })
+    }
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    
+   return draggableElements.reduce((closest, child)=>{
+       const box = child.getBoundingClientRect()
+       const  offset = y - box.top - box.height / 2
+       
+      if(offset < 0 && offset > closest.offset){
+          return {offset: offset, element: child}
+      } else{
+          return closest
+      }
+
+    },{offset: Number.NEGATIVE_INFINITY} ).element
 }
+   
+
+
+
+
+
+
+
+
+
+
+
+   let str = "hi may name is khan"
+
+    let newStr='';
+    for(let char of str){
+        if(char >= 'A' && char <= 'Z'){
+            newStr+= char.toLowerCase();
+
+        }else{
+            newStr+= char.toUpperCase();
+        }
+    }
+    
+
+
+
+
 
 
 // :::::::::::::::::::::::::::::::::::::::::::::::::::
